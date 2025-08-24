f1baeaaa31f90e3e835dc85c1be5c7514e4da512
digvijay315 <digvijaykumar.315@gmail.com>
2025-08-24 14:21:01 +0530

    crm fixed

--- crm frontend/src/api.js
+++ crm frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance=axios.create({
    
-          baseURL:'https://api.bharatproperties.co/'
+          baseURL:'https://newapi.bharatproperties.co/'
         //   baseURL:'http://localhost:5000/'
 
        
